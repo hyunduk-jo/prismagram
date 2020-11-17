85bf55515f017b2d7a7666ac7db17942c63aae25
hyunduk-jo <johd321@kakao.com>
2020-11-17 22:53:04 +0900

    add SendMessage SeeFeed SeeRooms

---
+++ src/api/Room/seeRooms/seeRooms.js
@@ -0,0 +1,18 @@
+import { UserInputError } from "apollo-server";
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    seeRooms: (_, __, { request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      return prisma.rooms({
+        where: {
+          participants_some: {
+            id: user.id
+          }
+        }
+      })
+    }
+  }
+}
