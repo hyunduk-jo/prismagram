e5b84bfca244b41780b8a7fdd696ebcb71cf0787
hyunduk-jo <johd321@kakao.com>
2020-11-19 20:18:45 +0900

    Update User.js

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -23,6 +23,10 @@ export default {
       return user.id === parentId;
     },
     posts: parent => prisma.user({ id: parent.id }).posts(),
-    rooms: parent => prisma.user({ id: parent.id }).rooms()
+    rooms: parent => prisma.user({ id: parent.id }).rooms(),
+    following: parent => prisma.user({ id: parent.id }).following(),
+    followers: parent => prisma.user({ id: parent.id }).followers(),
+    likes: parent => prisma.user({ id: parent.id }).likes(),
+    comments: parent => prisma.user({ id: parent.id }).comments()
   }
 }
